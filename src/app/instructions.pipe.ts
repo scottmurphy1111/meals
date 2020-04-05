97a00685535c196ba9187b1ca849fb165aa92175
scottmurphy1111 <scottmurphy1111@gmail.com>
2020-04-05 19:31:30 -0400

    styling meals

--- src/app/instructions.pipe.ts
+++ src/app/instructions.pipe.ts
@@ -7,7 +7,7 @@ export class InstructionsPipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
     console.log('value', value);
-    const newVal = value.split('.').join('.<br/><br />');
+    const newVal = value.split('.').join('.</li><li>');
     console.log('new val', newVal);
     return newVal;
   }
